f9dade4e0c47c17a170dcc8d0973545387ed6764
Justin Strang <Justinstrang100@gmail.com>
2022-09-09 17:03:34 -0400

    Querying/insertion/deletion working

--- dbConfig.js
+++ dbConfig.js
@@ -1,16 +1,29 @@
 const mySQL = require('mysql');
 
-const connection = mySQL.createConnection({
+const con = mySQL.createConnection({
     host: 'localhost',
     user: "root",
     password: "password",
     database: "usersDB",
 })
 
-connection.connect((err) => {
+con.connect((err) => {
     if (err) {
         throw err
     }
-    console.log('CONNECTED')
+    let sql = "DELETE FROM users WHERE username = 'John'";
+    con.query(sql, (err) => {
+        if (err) {
+            throw err
+        }
+        console.log('Inserted');
+    })
+    let sql2 = "SELECT * FROM users";
+    con.query(sql2, (err, result, fields) => {
+        if (err) {
+            throw err
+        }
+        console.log(result);
+    })
 })
 
